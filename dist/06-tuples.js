ca8c61c8004ed99da9e440f074c464ccda0c1f92
Khnykin Andrey <beyondmyspace@yandex.ru>
2023-07-07 20:53:57 +0300

    update .gitignore

--- dist/06-tuples.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// кортеж
-const pairs = [['key', 'value'], ['key2', 'value2']];
-const data = [1, true, 'lodash'];
-// может быть полезно для csv
-const doc = [];
-doc.push(['Andrey', 'space', 25, new Date(1993, 10, 1)]);
